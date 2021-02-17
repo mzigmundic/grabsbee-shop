5d9ec820c5dedce09a811dba8db919d26983c25d
mzxrandom <mzx.random@gmail.com>
2021-02-17 01:45:47 +0100

    Project cleanup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Search Elements */
 const triggerSearchOpen = document.getElementById("trigger-search-open");
 const triggerSearchClose = document.getElementById("trigger-search-close");
-const searchContainer = document.querySelector(".header__search");
+const searchContainer = document.querySelector(".search");
 
 /* Cart Elements */
 const triggerCartOpen = document.getElementById("trigger-cart-open");
@@ -9,54 +9,58 @@ const triggerCartClose = document.getElementById("trigger-cart-close");
 const cartContainer = document.querySelector(".cart");
 
 /* Main Navigation */
-const triggerMainNavOpen = document.getElementById("trigger-menu-open");
-const triggerMainNavClose = document.getElementById("trigger-menu-close");
-const mainNavContainer = document.querySelector(".main-nav");
+const triggerNavigationOpen = document.getElementById("trigger-menu-open");
+const triggerNavigationClose = document.getElementById("trigger-menu-close");
+const navigationContainer = document.querySelector(".navigation");
 
 /* Accordion */
-const accordionLevelOneHeadings = Array.from(document.querySelectorAll(".accordion__item-level-1-heading"));
-const accordionLevelTwoHeadings = Array.from(document.querySelectorAll(".accordion__item-level-2-heading"));
-
-/* Footer Summaries */
-const summaryDetails = Array.from(document.querySelectorAll(".footer__summaries-details-summary"));
+const accordionHeadings = Array.from(document.querySelectorAll(".accordion-heading"));
 
 /* Showcase Elements */
-const showcaseList = document.querySelector(".showcase__list");
+const showcaseList = document.querySelector(".showcase__items");
 const showcaseArrowLeft = document.getElementById("showcase-arrow-left");
 const showcaseArrowRight = document.getElementById("showcase-arrow-right");
-const showcaseIndicators = Array.from(document.querySelectorAll(".showcase__nav-item"));
-const numOfPics = document.querySelectorAll(".showcase__list-item").length;
+const showcaseIndicators = Array.from(document.querySelectorAll(".showcase__indicator-item"));
 
 /* Script Variables */
 const breakPoint = 1024;
+const numOfPics = showcaseList.querySelectorAll(".showcase__item").length;
 let showcaseIndex;
 let browserWidth;
 checkAppereance();
 setEventListeners();
 
 /* Functions */
-function hide(element) {
-    element.classList.add("visually-hidden");
+function activate(element) {
+    element.classList.add("active");
+}
+
+function deactivate(element) {
+    element.classList.remove("active");
 }
 
 function show(element) {
-    element.classList.remove("visually-hidden");
+    element.classList.add("visible");
 }
 
-function disableMainPageScroll() {
-    document.body.classList.add("modal-open");
+function hide(element) {
+    element.classList.remove("visible");
 }
 
-function enableMainPageScroll() {
-    document.body.classList.remove("modal-open");
+function disableBodyScroll() {
+    document.body.classList.add("no-scroll");
+}
+
+function enableBodyScroll() {
+    document.body.classList.remove("no-scroll");
 }
 
 function handleShowcaseArrowRight() {
     if (showcaseIndex < numOfPics - 1) {
         showcaseIndex++;
         showcaseList.style.transform = `translateX(-${showcaseIndex * browserWidth}px)`;
-        showcaseIndicators[showcaseIndex - 1].classList.remove("active");
-        showcaseIndicators[showcaseIndex].classList.add("active");
+        deactivate(showcaseIndicators[showcaseIndex - 1]);
+        activate(showcaseIndicators[showcaseIndex]);
     }
 }
 
@@ -64,39 +68,8 @@ function handleShowcaseArrowLeft() {
     if (showcaseIndex > 0) {
         showcaseIndex--;
         showcaseList.style.transform = `translateX(-${showcaseIndex * browserWidth}px)`;
-        showcaseIndicators[showcaseIndex + 1].classList.remove("active");
-        showcaseIndicators[showcaseIndex].classList.add("active");
-    }
-}
-
-function handleFooterSummaryClick(summary, e) {
-    if (summary.contains(e.target)) {
-        const icon = summary.getElementsByTagName("i")[0];
-        if (icon.classList.contains("icon-chevron-down")) {
-            icon.classList.remove("icon-chevron-down");
-            icon.classList.add("icon-chevron-up");
-        } else {
-            icon.classList.remove("icon-chevron-up");
-            icon.classList.add("icon-chevron-down");
-        }
-    }
-}
-
-function updateSummariesAppereance() {
-    if (window.innerWidth >= breakPoint) {
-        summaryDetails.forEach((summary) => {
-            summary.parentElement.setAttribute("open", true);
-            summary.classList.add("details-disabled-on-large");
-            summary.addEventListener("click", (e) => e.target);
-        });
-    } else {
-        summaryDetails.forEach((summary) => {
-            summary.parentElement.removeAttribute("open");
-            summary.getElementsByTagName("i")[0].classList.remove("icon-chevron-up");
-            summary.getElementsByTagName("i")[0].classList.add("icon-chevron-down");
-            summary.classList.remove("details-disabled-on-large");
-            summary.removeEventListener("click", (e) => e.target);
-        });
+        deactivate(showcaseIndicators[showcaseIndex + 1]);
+        activate(showcaseIndicators[showcaseIndex]);
     }
 }
 
@@ -106,15 +79,14 @@ function resetAllPageValues() {
     if (showcaseList) {
         showcaseList.style.transform = `translateX(0)`;
         showcaseIndicators.forEach((indicator) => {
-            indicator.classList.remove("active");
+            deactivate(indicator);
         });
-        showcaseIndicators[0].classList.add("active");
+        activate(showcaseIndicators[0]);
     }
 }
 
 function checkAppereance() {
     resetAllPageValues();
-    updateSummariesAppereance();
 }
 
 function setEventListeners() {
@@ -125,51 +97,36 @@ function setEventListeners() {
         showcaseArrowLeft.addEventListener("click", handleShowcaseArrowLeft);
     }
 
-    triggerSearchOpen.addEventListener("click", () => show(searchContainer));
-    triggerSearchClose.addEventListener("click", () => hide(searchContainer));
+    triggerSearchOpen.addEventListener("click", () => activate(searchContainer));
+    triggerSearchClose.addEventListener("click", () => deactivate(searchContainer));
 
     triggerCartOpen.addEventListener("click", () => {
-        show(cartContainer);
-        disableMainPageScroll();
+        activate(cartContainer);
+        disableBodyScroll();
     });
     triggerCartClose.addEventListener("click", () => {
-        hide(cartContainer);
-        enableMainPageScroll();
+        deactivate(cartContainer);
+        enableBodyScroll();
     });
 
-    triggerMainNavOpen.addEventListener("click", () => {
-        show(mainNavContainer);
-        disableMainPageScroll();
+    triggerNavigationOpen.addEventListener("click", () => {
+        activate(navigationContainer);
+        disableBodyScroll();
     });
-    triggerMainNavClose.addEventListener("click", () => {
-        hide(mainNavContainer);
-        enableMainPageScroll();
-    });
-
-    summaryDetails.forEach((summary) => {
-        summary.addEventListener("click", (e) => handleFooterSummaryClick(summary, e));
+    triggerNavigationClose.addEventListener("click", () => {
+        deactivate(navigationContainer);
+        enableBodyScroll();
     });
 
-    accordionLevelOneHeadings.forEach((acc) => {
-        acc.addEventListener("click", (e) => {
-            if (acc.contains(e.target) && !acc.classList.contains("active")) {
-                acc.classList.add("active");
-                show(acc.parentElement.querySelector(".accordion__item-level-1-content"));
-            } else if (acc.contains(e.target) && acc.classList.contains("active")) {
-                acc.classList.remove("active");
-                hide(acc.parentElement.querySelector(".accordion__item-level-1-content"));
-            }
-        });
-    });
-
-    accordionLevelTwoHeadings.forEach((acc) => {
-        acc.addEventListener("click", (e) => {
-            if (acc.contains(e.target) && !acc.classList.contains("active")) {
-                acc.classList.add("active");
-                show(acc.parentElement.querySelector(".accordion__item-level-2-content"));
-            } else if (acc.contains(e.target) && acc.classList.contains("active")) {
-                acc.classList.remove("active");
-                hide(acc.parentElement.querySelector(".accordion__item-level-2-content"));
+    accordionHeadings.forEach((accordionHead) => {
+        accordionHead.addEventListener("click", (e) => {
+            accordionContent = accordionHead.parentElement.querySelector(":scope > .accordion-content");
+            if (accordionHead.contains(e.target) && !accordionHead.classList.contains("active")) {
+                activate(accordionHead);
+                show(accordionContent);
+            } else if (accordionHead.contains(e.target) && accordionHead.classList.contains("active")) {
+                deactivate(accordionHead);
+                hide(accordionContent);
             }
         });
     });
