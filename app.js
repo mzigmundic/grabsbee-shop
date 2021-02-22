6625ea226178c6e1d3a2121ddf449da3ca29b226
mzxrandom <mzx.random@gmail.com>
2021-02-22 03:18:00 +0100

    Refactoring JS to module pattern

--- app.js
+++ app.js
@@ -1,218 +1,212 @@
-/* Search Elements */
-const triggerSearchOpen = document.getElementById("trigger-search-open");
-const triggerSearchClose = document.getElementById("trigger-search-close");
-const searchContainer = document.querySelector(".search");
+const Application = (function () {
+    const appVariables = {
+        breakPoint: 1024,
+        showcaseIndex: 0,
+    };
 
-/* Cart Elements */
-const triggerCartOpen = document.getElementById("trigger-cart-open");
-const triggerCartClose = document.getElementById("trigger-cart-close");
-const cartContainer = document.querySelector(".cart");
+    const DOM = {
+        triggerSearchOpen: document.getElementById("trigger-search-open"),
+        triggerSearchClose: document.getElementById("trigger-search-close"),
+        searchContainer: document.querySelector(".search"),
+        triggerCartOpen: document.getElementById("trigger-cart-open"),
+        triggerCartClose: document.getElementById("trigger-cart-close"),
+        cartContainer: document.querySelector(".cart"),
+        triggerNavigationOpen: document.getElementById("trigger-menu-open"),
+        triggerNavigationClose: document.getElementById("trigger-menu-close"),
+        navigationContainer: document.querySelector(".nav"),
+        accordionHeadings: Array.from(document.querySelectorAll(".accordion-heading")),
+        footerDetails: Array.from(document.querySelectorAll(".footer__details")),
+        showcaseList: document.querySelector(".showcase__items"),
+        showcaseArrowLeft: document.getElementById("showcase-arrow-left"),
+        showcaseArrowRight: document.getElementById("showcase-arrow-right"),
+        showcaseIndicators: Array.from(document.querySelectorAll(".showcase__indicator-item")),
+        triggerShopByOpen: document.getElementById("trigger-shopby-open"),
+        triggerShopByClose: document.getElementById("trigger-shopby-close"),
+        shoppingOptionsFilters: document.querySelector(".shopping-options__filters"),
+        viewAsGrid: document.getElementById("view-as-grid"),
+        viewAsList: document.getElementById("view-as-list"),
+        productsGrid: document.querySelector(".products__grid"),
+        descriptionTabHeadings: document.querySelectorAll("[data-tab-heading]"),
+        descriptionTabContents: document.querySelectorAll("[data-tab-content]"),
+    };
 
-/* Main Navigation */
-const triggerNavigationOpen = document.getElementById("trigger-menu-open");
-const triggerNavigationClose = document.getElementById("trigger-menu-close");
-const navigationContainer = document.querySelector(".nav");
+    const setEventListeners = function () {
+        window.addEventListener("resize", checkShowcaseAppereance);
+        window.matchMedia(`(min-width: ${appVariables.breakPoint}px)`).addEventListener("change", handleWidescreen);
 
-/* Accordion */
-const accordionHeadings = Array.from(document.querySelectorAll(".accordion-heading"));
+        DOM.triggerSearchOpen.addEventListener("click", () => activate(DOM.searchContainer));
+        DOM.triggerSearchClose.addEventListener("click", () => deactivate(DOM.searchContainer));
 
-/* Footer */
-const footerDetails = Array.from(document.querySelectorAll(".footer__details"));
-
-/* Home Page */
-const showcaseList = document.querySelector(".showcase__items");
-const showcaseArrowLeft = document.getElementById("showcase-arrow-left");
-const showcaseArrowRight = document.getElementById("showcase-arrow-right");
-const showcaseIndicators = Array.from(document.querySelectorAll(".showcase__indicator-item"));
-
-/* Subcategory Page */
-const triggerShopByOpen = document.getElementById("trigger-shopby-open");
-const triggerShopByClose = document.getElementById("trigger-shopby-close");
-const shoppingOptionsFilters = document.querySelector(".shopping-options__filters");
-const viewAsGrid = document.getElementById("view-as-grid");
-const viewAsList = document.getElementById("view-as-list");
-const productsGrid = document.querySelector(".products__grid");
-
-/* Product Page */
-const descriptionTabHeadings = document.querySelectorAll("[data-tab-heading]");
-const descriptionTabContents = document.querySelectorAll("[data-tab-content]");
-
-/* Script Variables */
-const breakPoint = 1024;
-const mediaQuery = window.matchMedia(`(min-width: ${breakPoint}px)`);
-let showcaseIndex;
-
-handleWidescreenChange(mediaQuery);
-checkShowcaseAppereance();
-setEventListeners();
-
-/* Functions */
-function activate(element) {
-    element.classList.add("active");
-}
-
-function deactivate(element) {
-    element.classList.remove("active");
-}
-
-function show(element) {
-    element.classList.add("visible");
-}
-
-function hide(element) {
-    element.classList.remove("visible");
-}
-
-function disableBodyScroll() {
-    document.body.classList.add("no-scroll");
-}
-
-function enableBodyScroll() {
-    document.body.classList.remove("no-scroll");
-}
-
-function preventer(event) {
-    event.preventDefault();
-}
-
-function handleShowcaseArrowRight() {
-    if (showcaseIndex < showcaseList.querySelectorAll(".showcase__item").length - 1) {
-        showcaseIndex++;
-        showcaseList.style.transform = `translateX(-${showcaseIndex * document.body.clientWidth}px)`;
-        deactivate(showcaseIndicators[showcaseIndex - 1]);
-        activate(showcaseIndicators[showcaseIndex]);
-    }
-}
-
-function handleShowcaseArrowLeft() {
-    if (showcaseIndex > 0) {
-        showcaseIndex--;
-        showcaseList.style.transform = `translateX(-${showcaseIndex * document.body.clientWidth}px)`;
-        deactivate(showcaseIndicators[showcaseIndex + 1]);
-        activate(showcaseIndicators[showcaseIndex]);
-    }
-}
-
-function handleWidescreenChange(e) {
-    if (e.matches) {
-        footerDetails.forEach((fd) => {
-            fd.open = true;
-            fd.querySelector(".footer__details-head").addEventListener("click", preventer);
-        });
-        accordionHeadings.forEach((ah) => {
-            deactivate(ah);
-            hide(ah.parentElement.querySelector(":scope > .accordion-content"));
-        });
-        enableBodyScroll();
-    } else {
-        footerDetails.forEach((fd) => {
-            fd.open = false;
-            fd.querySelector(".footer__details-head").removeEventListener("click", preventer);
-        });
-    }
-}
-
-function handleAccordion(e) {
-    accordionContent = this.parentElement.querySelector(":scope > .accordion-content");
-    if (
-        this.contains(e.target) &&
-        !this.classList.contains("active") &&
-        ((!this.parentElement.classList.contains("nav__category-item") &&
-            !this.parentElement.classList.contains("nav__subcategory-item")) ||
-            !mediaQuery.matches)
-    ) {
-        activate(this);
-        show(accordionContent);
-    } else if (this.contains(e.target)) {
-        deactivate(this);
-        hide(accordionContent);
-    }
-}
-
-function checkShowcaseAppereance() {
-    showcaseIndex = 0;
-    if (showcaseList) {
-        showcaseList.style.transform = `translateX(0)`;
-        showcaseIndicators.forEach((indicator) => {
-            deactivate(indicator);
-        });
-        activate(showcaseIndicators[0]);
-    }
-}
-
-function handleDescriptionTab() {
-    const th = document.querySelector(this.dataset.tabHeading);
-    descriptionTabContents.forEach((tc) => {
-        deactivate(tc);
-    });
-    descriptionTabHeadings.forEach((t) => {
-        deactivate(t);
-    });
-    activate(this);
-    activate(th);
-}
-
-function setEventListeners() {
-    window.addEventListener("resize", checkShowcaseAppereance);
-    mediaQuery.addEventListener("change", handleWidescreenChange);
-
-    triggerSearchOpen.addEventListener("click", () => activate(searchContainer));
-    triggerSearchClose.addEventListener("click", () => deactivate(searchContainer));
-
-    triggerCartOpen.addEventListener("click", () => {
-        activate(cartContainer);
-        disableBodyScroll();
-    });
-    triggerCartClose.addEventListener("click", () => {
-        deactivate(cartContainer);
-        enableBodyScroll();
-    });
-
-    triggerNavigationOpen.addEventListener("click", () => {
-        activate(navigationContainer);
-        disableBodyScroll();
-    });
-    triggerNavigationClose.addEventListener("click", () => {
-        deactivate(navigationContainer);
-        enableBodyScroll();
-    });
-
-    accordionHeadings.forEach((ah) => {
-        ah.addEventListener("click", handleAccordion);
-    });
-
-    if (showcaseList) {
-        showcaseArrowRight.addEventListener("click", handleShowcaseArrowRight);
-        showcaseArrowLeft.addEventListener("click", handleShowcaseArrowLeft);
-    }
-
-    if (triggerShopByOpen && triggerShopByClose) {
-        triggerShopByOpen.addEventListener("click", () => {
-            activate(shoppingOptionsFilters);
+        DOM.triggerCartOpen.addEventListener("click", () => {
+            activate(DOM.cartContainer);
             disableBodyScroll();
         });
-        triggerShopByClose.addEventListener("click", () => {
-            deactivate(shoppingOptionsFilters);
+        DOM.triggerCartClose.addEventListener("click", () => {
+            deactivate(DOM.cartContainer);
             enableBodyScroll();
         });
-    }
 
-    if (viewAsGrid && viewAsList) {
-        viewAsGrid.addEventListener("click", () => {
-            productsGrid.classList.remove("list");
-            deactivate(viewAsList);
-            activate(viewAsGrid);
+        DOM.triggerNavigationOpen.addEventListener("click", () => {
+            activate(DOM.navigationContainer);
+            disableBodyScroll();
         });
-        viewAsList.addEventListener("click", () => {
-            productsGrid.classList.add("list");
-            deactivate(viewAsGrid);
-            activate(viewAsList);
+        DOM.triggerNavigationClose.addEventListener("click", () => {
+            deactivate(DOM.navigationContainer);
+            enableBodyScroll();
         });
-    }
 
-    if (descriptionTabHeadings) {
-        descriptionTabHeadings.forEach((tab) => {
-            tab.addEventListener("click", handleDescriptionTab);
+        DOM.accordionHeadings.forEach((ah) => {
+            ah.addEventListener("click", handleAccordion);
         });
-    }
-}
+
+        if (DOM.showcaseList) {
+            DOM.showcaseArrowRight.addEventListener("click", handleShowcaseArrowRight);
+            DOM.showcaseArrowLeft.addEventListener("click", handleShowcaseArrowLeft);
+        }
+
+        if (DOM.triggerShopByOpen && DOM.triggerShopByClose) {
+            DOM.triggerShopByOpen.addEventListener("click", () => {
+                activate(DOM.shoppingOptionsFilters);
+                disableBodyScroll();
+            });
+            DOM.triggerShopByClose.addEventListener("click", () => {
+                deactivate(DOM.shoppingOptionsFilters);
+                enableBodyScroll();
+            });
+        }
+
+        if (DOM.viewAsGrid && DOM.viewAsList) {
+            DOM.viewAsGrid.addEventListener("click", () => {
+                DOM.productsGrid.classList.remove("list");
+                deactivate(DOM.viewAsList);
+                activate(DOM.viewAsGrid);
+            });
+            DOM.viewAsList.addEventListener("click", () => {
+                DOM.productsGrid.classList.add("list");
+                deactivate(DOM.viewAsGrid);
+                activate(DOM.viewAsList);
+            });
+        }
+
+        if (DOM.descriptionTabHeadings) {
+            DOM.descriptionTabHeadings.forEach((tab) => {
+                tab.addEventListener("click", handleDescriptionTab);
+            });
+        }
+    };
+
+    const activate = function (element) {
+        element.classList.add("active");
+    };
+
+    const deactivate = function (element) {
+        element.classList.remove("active");
+    };
+
+    const show = function (element) {
+        element.classList.add("visible");
+    };
+
+    const hide = function (element) {
+        element.classList.remove("visible");
+    };
+
+    const disableBodyScroll = function () {
+        document.body.classList.add("no-scroll");
+    };
+
+    const enableBodyScroll = function () {
+        document.body.classList.remove("no-scroll");
+    };
+
+    const preventer = function (event) {
+        event.preventDefault();
+    };
+
+    const handleShowcaseArrowRight = function () {
+        if (appVariables.showcaseIndex < DOM.showcaseList.querySelectorAll(".showcase__item").length - 1) {
+            appVariables.showcaseIndex++;
+            DOM.showcaseList.style.transform = `translateX(-${appVariables.showcaseIndex * document.body.clientWidth}px)`;
+            deactivate(DOM.showcaseIndicators[appVariables.showcaseIndex - 1]);
+            activate(DOM.showcaseIndicators[appVariables.showcaseIndex]);
+        }
+    };
+
+    const handleShowcaseArrowLeft = function () {
+        if (appVariables.showcaseIndex > 0) {
+            appVariables.showcaseIndex--;
+            DOM.showcaseList.style.transform = `translateX(-${appVariables.showcaseIndex * document.body.clientWidth}px)`;
+            deactivate(DOM.showcaseIndicators[appVariables.showcaseIndex + 1]);
+            activate(DOM.showcaseIndicators[appVariables.showcaseIndex]);
+        }
+    };
+
+    const checkShowcaseAppereance = function () {
+        appVariables.showcaseIndex = 0;
+        if (DOM.showcaseList) {
+            DOM.showcaseList.style.transform = `translateX(0)`;
+            DOM.showcaseIndicators.forEach((indicator) => {
+                deactivate(indicator);
+            });
+            activate(DOM.showcaseIndicators[0]);
+        }
+    };
+
+    const handleWidescreen = function (e) {
+        if (e.matches) {
+            DOM.footerDetails.forEach((fd) => {
+                fd.open = true;
+                fd.querySelector(".footer__details-head").addEventListener("click", preventer);
+            });
+            DOM.accordionHeadings.forEach((ah) => {
+                deactivate(ah);
+                hide(ah.parentElement.querySelector(":scope > .accordion-content"));
+            });
+            enableBodyScroll();
+        } else {
+            DOM.footerDetails.forEach((fd) => {
+                fd.open = false;
+                fd.querySelector(".footer__details-head").removeEventListener("click", preventer);
+            });
+        }
+    };
+
+    const handleAccordion = function (e) {
+        accordionContent = this.parentElement.querySelector(":scope > .accordion-content");
+        if (
+            this.contains(e.target) &&
+            !this.classList.contains("active") &&
+            ((!this.parentElement.classList.contains("nav__category-item") &&
+                !this.parentElement.classList.contains("nav__subcategory-item")) ||
+                !window.matchMedia(`(min-width: ${appVariables.breakPoint}px)`).matches)
+        ) {
+            activate(this);
+            show(accordionContent);
+        } else if (this.contains(e.target)) {
+            deactivate(this);
+            hide(accordionContent);
+        }
+    };
+
+    const handleDescriptionTab = function () {
+        const th = document.querySelector(this.dataset.tabHeading);
+        DOM.descriptionTabContents.forEach((tc) => {
+            deactivate(tc);
+        });
+        DOM.descriptionTabHeadings.forEach((t) => {
+            deactivate(t);
+        });
+        activate(this);
+        activate(th);
+    };
+
+    return {
+        initialize: function () {
+            handleWidescreen(window.matchMedia(`(min-width: ${appVariables.breakPoint}px)`));
+            checkShowcaseAppereance();
+            setEventListeners();
+        },
+    };
+})();
+
+Application.initialize();
